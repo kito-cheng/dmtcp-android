b78c940b2633563dfdd58a5091db68766c50b4f4
Kito Cheng <kito@0xlab.org>
2012-03-23 15:15:37 +0800

    Android.mk for jalib

---
+++ dmtcp/jalib/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := external/stlport/stlport \
+                    bionic/ \
+                    external/dmtcp/dmtcp/src
+LOCAL_CFLAGS+= -O0 -g3 -DHAVE_CONFIG_H
+LOCAL_SRC_FILES:= jalib.cpp jassert.cpp jfilesystem.cpp \
+                  jsocket.cpp jalloc.cpp  jbuffer.cpp \
+                  jserialize.cpp jtimer.cpp
+
+LOCAL_MODULE := libjal
+LOCAL_SHARED_LIBRARIES := libstlport
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
