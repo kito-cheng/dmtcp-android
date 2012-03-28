d87018f0617c97681ea82555c5d6fd3e3b0607b7
Kito Cheng <kito@0xlab.org>
2012-03-28 14:18:42 +0800

    Add more test case for DMTCP
     - Testcase for ashmem

---
+++ test/Android.mk
@@ -0,0 +1,53 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := testdmtcp-ashmem.c
+LOCAL_CFLAGS+= -O0 -g3 -DDEBUG -DTIMING
+LOCAL_MODULE := testdmtcp-ashmem
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := dmtcp1.c
+LOCAL_CFLAGS+= -O0 -g3 -DDEBUG -DTIMING
+LOCAL_MODULE := dmtcp1
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := dmtcp2.c
+LOCAL_CFLAGS+= -O0 -g3 -DDEBUG -DTIMING
+LOCAL_MODULE := dmtcp2
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := dmtcp3.c
+LOCAL_CFLAGS+= -O3 -g3 -DDEBUG -DTIMING
+LOCAL_MODULE := dmtcp3
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := dmtcp4.c
+LOCAL_CFLAGS+= -O0 -g3 -DDEBUG -DTIMING
+LOCAL_MODULE := dmtcp4
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := dmtcp5.c
+LOCAL_CFLAGS+= -O0 -g3 -DDEBUG -DTIMING
+LOCAL_MODULE := dmtcp5
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := external/dmtcp/dmtcp/src
+LOCAL_SRC_FILES := dmtcpaware1.c
+LOCAL_CFLAGS+= -O0 -g3 -DDEBUG -DTIMING
+LOCAL_SHARED_LIBRARIES := libdmtcpaware
+LOCAL_MODULE := dmtcpaware1
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
